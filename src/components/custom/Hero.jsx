a17d28aefc8e43e80b25b7364435cddce0e0e235
banasmita24 <banasmitajena2405@gmail.com>
2025-05-15 06:47:57 +0530

    Resolved merge conflicts

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -8,11 +8,7 @@ function Hero() {
       <h1
       className="font-extrabold text-[50px] text-center mt-16">
         <span className='text-[#f56551]'>🌍 Discover Your Next Adventure with the Power of AI:</span> <br></br> Your Journey, Your Way</h1>
-<<<<<<< HEAD
-        <p className='text-xl text-gray-500 text-center'>Say goodbye to generic travel plans! Let your personal AI trip planner craft unforgettable experiences with custom itineraries </p>
-=======
         <p className='text-xl text-gray-500 text-center'>Say goodbye to generic travel plans! Let your personal AI trip planner craft unforgettable experiences with custom itineraries — all tailored to your interests, style, and budget.</p>
->>>>>>> ecd7b51f80daeec28cacbc1db96fead90c8da19e
         <Link to={'/create-trip'}>
           <Button className="!bg-white !text-black !shadow !border !border-gray-300 rounded-full">Get Started, It's Free.</Button>
         </Link>
